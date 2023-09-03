52fc33c7eb66529fd2c83c56fbbec07335736880
NECT7 <www.melvintobar1@gmail.com>
2023-09-03 09:55:30 -0600

    Validación de espacios vacíos
    Se valida que los espacios no estén vacíos

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,15 @@ namespace ControlVersion
         private void btnEjecutar_Click(object sender, EventArgs e)
         {
             {
-                //
+                // Validar que los campos no estén vacíos
+                if (string.IsNullOrWhiteSpace(txtNombreCompleto.Text) ||
+                    string.IsNullOrWhiteSpace(txtCarnet.Text) ||
+                    string.IsNullOrWhiteSpace(txtGrupoTeorico.Text) ||
+                    cmbPrograma.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Por favor, complete todos los campos antes de continuar.", "Error de validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return; // Salir del evento si hay campos vacíos
+                }
             }
         }
 
